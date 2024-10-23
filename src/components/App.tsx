e3ab80cd5183fce4e4f23e216a8c0e0a29bd4114
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-23 14:42:39 +0200

    the maximum number of images is taken into account

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,6 +12,8 @@ function App() {
   const [images, setImages] = useState<Image[]>([]);
   const [query, setQuery] = useState<string>('');
   const [page, setPage] = useState<number>(1);
+  const [perPage, setPerPage] = useState<number>(200);
+  const [total, setTotal] = useState<number>(0);
   const [loadMore, setLoadMore] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const isFirstRender = useRef(true);
@@ -25,12 +27,27 @@ function App() {
     async function reqApi() {
       setIsLoading(true);
 
+      let restImage = 0;
+
+      if (total !== 0 && total < perPage) {
+        restImage = total;
+      }
+
       try {
-        const { hits } = await reqPixabay(query, page);
+        const { hits, totalHits } = await reqPixabay(
+          query,
+          page,
+          restImage > 0 ? restImage : perPage
+        );
+
+        if (total === 0) {
+          setTotal(totalHits);
+        }
 
         setImages(prev => [...prev, ...hits]);
       } catch (error) {
       } finally {
+        setTotal(prev => prev - (restImage > 0 ? restImage : perPage));
         setIsLoading(false);
         setLoadMore(true);
       }
@@ -54,7 +71,7 @@ function App() {
     <div className="App">
       <Searchbar onSubmit={onSubmit} />
       {images.length > 0 && <ImageGallery images={images} />}
-      {images.length > 0 && <Button title="Load More" onClick={nextPage} />}
+      {total > 0 && <Button title="Load More" onClick={nextPage} />}
       {isLoading && <Loader />}
     </div>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,9 +4,9 @@ const API_KEY = '36810234-b5e1d7960ec1148affe35137c';
 
 axios.defaults.baseURL = `https://pixabay.com/api`;
 
-export default async function reqPixabay(q: string, page: number) {
+export default async function reqPixabay(q: string, page: number, perPage: number) {
   const response = await axios.get(
-    `?key=${API_KEY}&q=${q}&page=${page}&per_page=12&image_type=photo&orientation=horizontal`
+    `?key=${API_KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal`
   );
   const data = await response.data;
 
