89d016f5a8a3ebc074d136666b2636ff7775f03d
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-27 08:43:45 +0100

    change style for ImageGallery

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,17 @@
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
+import { ReactNode } from 'react';
 
 const modalRoot: HTMLDivElement | null = document.querySelector('#modal-root');
 
-export default function Modal() {
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Modal({ children }: Props) {
   return createPortal(
     <div className={s.overlay}>
-      <div className={s.modal}>
-        <img src="" alt="" />
-      </div>
+      <div className={s.modal}>{children}</div>
     </div>,
     modalRoot!
   );
