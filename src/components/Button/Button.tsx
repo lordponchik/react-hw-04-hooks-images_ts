ab5f23227b388529008c539fa94e438f3c541f09
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-27 09:34:34 +0100

    add style for LoadMore

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,5 @@
+import s from './Button.module.css';
+
 interface Props {
   title: string;
   onClick: () => void;
@@ -5,7 +7,7 @@ interface Props {
 
 export default function Button({ title, onClick }: Props) {
   return (
-    <button type="button" onClick={onClick}>
+    <button type="button" className={s.button} onClick={onClick}>
       {title}
     </button>
   );
